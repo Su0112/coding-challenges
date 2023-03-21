3bb28398c02438ea7824123ef2d00ed7a95551ab
Su Shruthi <shruthacm@gmail.com>
2023-03-20 23:11:21 -0700

    added challenge 21

--- kata.js
+++ kata.js
@@ -20,3 +20,21 @@ if (lovefunc(flower1, flower2)) {
 } else {
   console.log("They are not in love :(");
 }
+
+//You get an array of numbers, return the sum of all of the positives ones.
+
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+//Note: if there is nothing to sum, the sum is default to 0.
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+let numbers = [1, -4, 7, 12];
+let result = positiveSum(numbers);
+console.log(result); // Output: 20
