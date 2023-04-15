7a68f924ab2f98c0fdf4d4eb56399333d66b26b9
Su Shruthi <shruthacm@gmail.com>
2023-04-15 16:11:26 -0700

    add challenge 12

--- kata.js
+++ kata.js
@@ -292,3 +292,47 @@ function isIsogram(str) {
 console.log(isIsogram("Dermatoglyphics")); // Output: true
 console.log(isIsogram("aba")); // Output: false
 console.log(isIsogram("moOse")); // Output: false
+
+//challege #12
+console.log("Challenge #12");
+"use strict";
+
+/* Question 00
+
+Write a converter that will change Celsius to Fahrenheit and back again.
+
+Your function should take in a number, and a boolean. The number will be the temperature in degrees, and the boolean will be whether to convert from C to F (true) or F to C (false). Your answer should be rounded to one decimal place, and returned as a Number, not a string.
+
+If the first argument is not a number, return NaN for the result.
+
+Examples:
+
+- tempConverter(32, true) returns 89.6 as a result
+- tempConverter(32, false) returns 0.0 as a result
+- tempConverter(98.6, false) returns 37 as a result
+- tempConverter("12", <anything>) returns NaN as a result
+
+*/
+
+const tempConverter = function(value, cToF) {
+
+  /* IMPLEMENT ME */
+  //If the first argument is not a number, return NaN for the result.
+  if (typeof value !== 'number') {
+    return NaN;
+  }
+  //Celsius to Fahrenheit
+  //If cToF is true, the function converts Celsius to Fahrenheit using the formula (C * 1.8) + 32
+  if (cToF) {
+    return +Number((value * 1.8 + 32).toFixed(1));
+  }
+  //Fahrenheit to Celsius
+  //If cToF is false, the function converts Fahrenheit to Celsius using the formula (F - 32) / 1.8
+  return +Number(((value - 32) / 1.8).toFixed(1));
+
+};
+
+
+// Don't change below:
+
+module.exports = { tempConverter };
