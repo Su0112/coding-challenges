0f5feaac3516f5a8d36c9323f974c642514dd427
Su Shruthi <shruthacm@gmail.com>
2023-04-15 16:15:25 -0700

    add challenge 15 and 16

--- kata.js
+++ kata.js
@@ -480,3 +480,53 @@ const range = function(count, skipZero = false, descending = false) {
 
 //challege 16
 console.log("Challenge #16");
+"use strict";
+
+/* Question 04
+
+Implement the function as defined below.
+
+Given an array of values, the minmax() function will return an array that contains the minimum and maximum values in the array, always with minimum at index 0 and maximum at index 1. For the purposes of this question, you are not allowed to use Math.max() or Math.min().
+
+The array can be a list of lower-cased strings instead of numbers. In this case, min is the string that would be sorted first alphabetically and max is the string that would be sorted last alphabetically ("a" < "b", while "ab" > "aa", and so on).
+
+Mixed-type (strings and numbers) arrays are not of concern to us (meaning, do not worry about this situation).
+
+Examples:
+
+- minmax([1, 2, 3, 4, 5]) returns [1, 5]
+- minmax([90, 89, 123, 3]) returns [3, 123]
+- minmax(["apple", "banana", "canada"]) returns ["apple", "canada"]
+- minmax([]) returns [undefined, undefined]
+
+*/
+
+const minmax = function(list) {
+  //if the array is an empty array
+  if (list.length === 0) {
+    return [undefined, undefined];
+  }
+  //initialize
+  let min = list[0];
+  let max = list[0];
+  //loop through the given array
+  for (let i = 0; i < list.length; i++) {
+    //current item in the array is defined as current
+    const current = list[i];
+    //conditionals to check min or max than the current
+    if (current < min) {
+      min = current;
+    } else if (current > max) {
+      max = current;
+
+    }
+
+  }
+  //return only min, max
+  return [min, max];
+};
+
+// Don't change below:
+
+module.exports = { minmax };
+
